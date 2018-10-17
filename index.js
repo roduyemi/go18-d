3487e7405fb5480270c375d68d9b15b437ed3e2a
rodu <remi.oduyemi@bbc.co.uk>
2018-10-17 13:36:37 +0100

    add cors back

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ app.use(express.json())
 
 const port = 8080;
 
-// const allowCrossDomain = (req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*')
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
-//   res.header('Access-Control-Allow-Headers', 'Content-Type')
-//   next();
-// }
+const allowCrossDomain = (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  res.header('Access-Control-Allow-Headers', 'Content-Type')
+  next();
+}
 
 app.use(allowCrossDomain)
 
